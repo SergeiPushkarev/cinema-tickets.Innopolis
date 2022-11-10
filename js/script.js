eea78852545277ee8b629b62a197b8e92a816bff
SergeiPushkarev <87870860+SergeiPushkarev@users.noreply.github.com>
2022-11-10 11:35:28 +0300

    T3-17 Modal window and initialize form

--- js/script.js
+++ js/script.js
@@ -24,20 +24,90 @@ $(document).ready(function(){
     });
   });
 
-//burger button toggle
+
 const brgBtn = document.getElementById('brgBtn');
 const mobMenu = document.getElementById('mobilemenu');
-function menuToggle(){
-    brgBtn.classList.toggle('burgermenu__btn-open')
-    mobMenu.classList.toggle('mobilemenu-open')
-}
-brgBtn.onclick = menuToggle;
-
-const btnGift = document.getElementById("popup_prize");
+const giftForm = document.getElementById("popup-prize");
 const btnGiftOpen = document.querySelector('#btn-gift');
 const btnGiftClose = document.querySelector('#popup-btn');
+const nameInputGift = document.querySelector("#popup-prize input[name='name']").parentNode;
+const emailInputGift = document.querySelector("#popup-prize input[name='email']").parentNode;
+const selectInputGigt = document.querySelector('#selectPrize');
+const INPUT_ERORR_CLASS = 'formlabel-error';
+const INPUT_FOCUS_CLASS = 'formlabel-focused';
+//burger button toggle
+function menuToggle(){
+    brgBtn.classList.toggle('burgermenu__btn-open');
+    mobMenu.classList.toggle('mobilemenu-open');
+}
+brgBtn.onclick = menuToggle;
+//giftbutton toggle
 function popupToggle () {
-    btnGift.classList.toggle('hidden')
+    giftForm.classList.toggle('hidden')
 };
-btnGiftOpen.onclick = popupToggle;
+btnGiftOpen.onclick = function() {
+    popupToggle();
+    nameField.focus();
+};
 btnGiftClose.onclick = popupToggle;
+
+
+function initializeField(field) {
+    const input = field.getElementsByTagName('input')[0];
+    const errorText = field.querySelector('.formlabel__error-msg');
+    clearForm();
+    input.value = "";
+    field.classList.remove(INPUT_FOCUS_CLASS);
+    function clearForm(){
+        field.classList.remove(INPUT_ERORR_CLASS);
+        errorText.innerText = "";
+    };
+    input.addEventListener('focus', function(){
+        field.classList.add(INPUT_FOCUS_CLASS)
+    });
+    input.addEventListener('input', clearForm);
+    input.addEventListener('blur', function(){
+        if (!input.value) {
+            field.classList.remove(INPUT_FOCUS_CLASS)
+        }
+    });
+    return {
+        focus(){
+            input.focus()
+        },
+        getValue(){
+            return input.value
+        },
+        setError(errorMsg){
+            errorText.innerText = errorMsg;
+            field.classList.add(INPUT_ERORR_CLASS);
+        }
+    }
+};
+const nameField = initializeField(nameInputGift);
+const emailField = initializeField(emailInputGift);
+
+
+
+giftForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nameValue = nameField.getValue();
+    const emailValue = emailField.getValue();
+    if (!nameValue) {
+        nameField.setError('Обязательно для заполнения');
+        nameField.focus();
+        return
+    };
+    if (selectInputGift.value === "none"){
+        selectInputGift.classList.add(INPUT_ERORR_CLASS);
+        return
+    };
+    const data = {
+        name: nameValue,
+        email: emailValue,
+        id: selectInputGift.value,
+    };
+    const url = new URL('https://jsonplaceholder.typicode.com/users');
+    url.search = new URLSearchParams(data).toString();
+    fetch(url.toString())
+})
--- target/script.js
+++ target/script.js
@@ -25,21 +25,89 @@ $(document).ready(function () {
     }
   });
 });
-
-//burger button toggle
 var brgBtn = document.getElementById('brgBtn');
 var mobMenu = document.getElementById('mobilemenu');
+var giftForm = document.getElementById("popup-prize");
+var btnGiftOpen = document.querySelector('#btn-gift');
+var btnGiftClose = document.querySelector('#popup-btn');
+var nameInputGift = document.querySelector("#popup-prize input[name='name']").parentNode;
+var emailInputGift = document.querySelector("#popup-prize input[name='email']").parentNode;
+var selectInputGigt = document.querySelector('#selectPrize');
+var INPUT_ERORR_CLASS = 'formlabel-error';
+var INPUT_FOCUS_CLASS = 'formlabel-focused';
+//burger button toggle
 function menuToggle() {
   brgBtn.classList.toggle('burgermenu__btn-open');
   mobMenu.classList.toggle('mobilemenu-open');
 }
 brgBtn.onclick = menuToggle;
-var btnGift = document.getElementById("popup_prize");
-var btnGiftOpen = document.querySelector('#btn-gift');
-var btnGiftClose = document.querySelector('#popup-btn');
+//giftbutton toggle
 function popupToggle() {
-  btnGift.classList.toggle('hidden');
+  giftForm.classList.toggle('hidden');
 }
 ;
-btnGiftOpen.onclick = popupToggle;
+btnGiftOpen.onclick = function () {
+  popupToggle();
+  nameField.focus();
+};
 btnGiftClose.onclick = popupToggle;
+function initializeField(field) {
+  var input = field.getElementsByTagName('input')[0];
+  var errorText = field.querySelector('.formlabel__error-msg');
+  clearForm();
+  input.value = "";
+  field.classList.remove(INPUT_FOCUS_CLASS);
+  function clearForm() {
+    field.classList.remove(INPUT_ERORR_CLASS);
+    errorText.innerText = "";
+  }
+  ;
+  input.addEventListener('focus', function () {
+    field.classList.add(INPUT_FOCUS_CLASS);
+  });
+  input.addEventListener('input', clearForm);
+  input.addEventListener('blur', function () {
+    if (!input.value) {
+      field.classList.remove(INPUT_FOCUS_CLASS);
+    }
+  });
+  return {
+    focus: function focus() {
+      input.focus();
+    },
+    getValue: function getValue() {
+      return input.value;
+    },
+    setError: function setError(errorMsg) {
+      errorText.innerText = errorMsg;
+      field.classList.add(INPUT_ERORR_CLASS);
+    }
+  };
+}
+;
+var nameField = initializeField(nameInputGift);
+var emailField = initializeField(emailInputGift);
+giftForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var nameValue = nameField.getValue();
+  var emailValue = emailField.getValue();
+  if (!nameValue) {
+    nameField.setError('Обязательно для заполнения');
+    nameField.focus();
+    return;
+  }
+  ;
+  if (selectInputGift.value === "none") {
+    selectInputGift.classList.add(INPUT_ERORR_CLASS);
+    return;
+  }
+  ;
+  var data = {
+    name: nameValue,
+    email: emailValue,
+    id: selectInputGift.value
+  };
+  var url = new URL('https://jsonplaceholder.typicode.com/users');
+  url.search = new URLSearchParams(data).toString();
+  fetch(url.toString());
+});
